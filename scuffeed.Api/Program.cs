02b11aaec8b3eef30ef4ae00f1b7fc944cec1aac
Atte Pitkänen <pitkasenatte@gmail.com>
2023-03-21 13:46:09 +0100

    adds user secrets

--- scuffeed.Api/Program.cs
+++ scuffeed.Api/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PostContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PostContext") ?? throw new InvalidOperationException("Connection string 'PostContext' not found.")));
+    options.UseSqlServer(builder.Configuration["DBKeyName"] ?? throw new InvalidOperationException("Connection string 'PostContext' not found.")));
 
 // Add services to the container.
 
@@ -23,7 +23,6 @@ app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-
     app.UseCors(cors =>
     {
         cors
@@ -40,3 +39,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
